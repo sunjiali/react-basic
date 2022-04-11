a3f0ad44f1394af62a4d30c546fa2bfe880e9147
Jiali Sun <275261506@qq.com>
2022-04-11 22:34:53 +0800

    first

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// React: 核心框架包
 ReactDOM.render(
   <React.StrictMode>
     <App />
